2a218643da2043e71e18c828856c491d59b44fe8
Szymon Nitecki <szymon.nn@gmail.com>
2019-07-11 16:50:59 +0200

    added speech service

--- ExampleApp/App.js
+++ ExampleApp/App.js
@@ -9,27 +9,51 @@
 const { GoogleSpeechApi } = NativeModules;
 
 import React, { Component } from 'react';
-import { Text, View, NativeModules, Button } from 'react-native';
+import { Text, View, NativeModules, Button, DeviceEventEmitter } from 'react-native';
 
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentText: "", 
+      previousTexts: "",
+      button: "Start listening"
+    };
+  }
+
   componentDidMount(){
-  	GoogleSpeechApi.setApiKey("Test");
+  	GoogleSpeechApi.setApiKey("ya29.c.ElpCB13RC3FXH8ZzSJZ7O6BibCJ2LYcqXt5P3BJgcd-gfwmFzRD181mGN2toGwaVuInF8DDahrvihaAo22Rf7OS5OHDUrmdz3vbeiAoL5ajBZKJaqGK5vgF3q9k")
+    DeviceEventEmitter.addListener('onSpeechRecognized', (event) => {
+      var previousTexts = this.state.previousTexts;
+      var currentText = event['text']
+      var button = "I'm listening"
+      if (event['isFinal']){
+        currentText = ""
+        previousTexts = event['text'] + "\n" + previousTexts;
+        button = "Start listening"
+      }
+      this.setState({
+        currentText: currentText,
+        previousTexts: previousTexts,
+        button: button
+      });
+      //GoogleSpeechApi.show(event['text'], 0)
+    });
   }
 
   render() {
     return (
-      <View>
+      <View style={{ margin: 30 }}>
+        <Text>{this.state.currentText}</Text>
+        <Text>{this.state.previousTexts}</Text>
         <Button
-          title={"start voice recorder"}
+          title={this.state.button}
           onPress={() => {
+              this.setState({
+                button: "I'm listening"
+              })
               GoogleSpeechApi.start()
-            }
-          }/>
-        <Button
-          title={"stop voice recorder"}
-          onPress={() => {
-              GoogleSpeechApi.stop()
             }
           }/>
       </View>
--- android/src/main/java/com/reactlibrary/GoogleSpeechApiModule.java
+++ android/src/main/java/com/reactlibrary/GoogleSpeechApiModule.java
@@ -1,22 +1,61 @@
 package com.reactlibrary;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.text.TextUtils;
 import android.util.Log;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.reactlibrary.speech_service.SpeechEvent;
+import com.reactlibrary.speech_service.SpeechService;
 import com.reactlibrary.voice_recorder.VoiceEvent;
 import com.reactlibrary.voice_recorder.VoiceRecorder;
 
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.disposables.Disposable;
 
 public class GoogleSpeechApiModule extends ReactContextBaseJavaModule {
 
     private static final String TAG = "GoogleSpeechApi";
+    private static final String KEY_TEXT = "text";
+    private static final String KEY_IS_FINAL = "isFinal";
+    private static final String ON_SPEECH_RECOGNIZED = "onSpeechRecognized";
 
     private VoiceRecorder voiceRecorder = new VoiceRecorder();
-    private Disposable voiceRecorderDisposable;
+    private SpeechService speechService;
+    private CompositeDisposable compositeDisposable;
+    private String apiKey;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            speechService = SpeechService.from(service);
+
+            compositeDisposable.add(
+                    speechService.getSpeechEventObservable()
+                            .subscribe(GoogleSpeechApiModule.this::handleSpeechEvent)
+            );
+            compositeDisposable.add(
+                    voiceRecorder.getVoiceEventObservable()
+                            .subscribe(GoogleSpeechApiModule.this::handleVoiceEvent)
+            );
+
+            voiceRecorder.start();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            speechService = null;
+        }
+    };
 
     public GoogleSpeechApiModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -25,24 +64,31 @@ public class GoogleSpeechApiModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void start() {
         Log.i(TAG, "start");
-        if (voiceRecorderDisposable != null){
-            voiceRecorderDisposable.dispose();
+        if (compositeDisposable != null) {
+            compositeDisposable.dispose();
         }
-        voiceRecorderDisposable =
-                voiceRecorder.getVoiceEventObservable().subscribe(this::handleVoiceEvent);
-        voiceRecorder.start();
+        compositeDisposable = new CompositeDisposable();
+        if (speechService == null) {
+            Intent serviceIntent = new Intent(getReactApplicationContext(), SpeechService.class);
+            getReactApplicationContext().bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        } else {
+            throw new RuntimeException("Another instance of SpeechService is already running");
+        }
     }
 
     @ReactMethod
     public void stop() {
         Log.i(TAG, "stop");
         voiceRecorder.stop();
-        voiceRecorderDisposable.dispose();
+        compositeDisposable.dispose();
+        getReactApplicationContext().unbindService(serviceConnection);
+        speechService = null;
     }
 
     @ReactMethod
     public void setApiKey(String apiKey) {
         Log.i(TAG, "setApiKey: " + apiKey);
+        this.apiKey = apiKey;
     }
 
     @Override
@@ -66,13 +112,51 @@ public class GoogleSpeechApiModule extends ReactContextBaseJavaModule {
 
     private void onVoiceStart() {
         Log.i(TAG, "onVoiceStart");
+        if (speechService != null) {
+            try {
+                speechService.startRecognizing(voiceRecorder.getSampleRate(), apiKey);
+            } catch (Exception e) {
+                speechService.startRecognizing(16000, apiKey);
+            }
+        }
     }
 
     private void onVoice(byte[] data, int size) {
         Log.i(TAG, "onVoice");
+        if (speechService != null) {
+            speechService.recognize(data, size);
+        }
     }
 
     private void onVoiceEnd() {
         Log.i(TAG, "onVoiceEnd");
+        if (speechService != null) {
+            speechService.finishRecognizing();
+        }
+    }
+
+    private void handleSpeechEvent(SpeechEvent speechEvent) {
+        Log.i(TAG, speechEvent.getText() + " " + speechEvent.isFinal());
+        WritableMap params = Arguments.createMap();
+
+        if (!TextUtils.isEmpty(speechEvent.getText())) {
+            params.putString(KEY_TEXT, speechEvent.getText());
+        } else {
+            params.putString(KEY_TEXT, "");
+        }
+
+        params.putBoolean(KEY_IS_FINAL, speechEvent.isFinal());
+        sendJSEvent(getReactApplicationContext(), ON_SPEECH_RECOGNIZED, params);
+        if (speechEvent.isFinal()) {
+            stop();
+        }
+    }
+
+    private void sendJSEvent(ReactContext reactContext,
+                           String eventName,
+                           WritableMap params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
     }
 }
