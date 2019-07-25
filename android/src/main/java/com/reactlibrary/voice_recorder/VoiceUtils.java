15b307e92edb7ce100ddf5ecbdb8997cb974f04f
Szymon Nitecki <szymon.nn@gmail.com>
2019-07-25 11:57:04 +0200

    added null check for buffer

--- android/src/main/java/com/reactlibrary/voice_recorder/VoiceUtils.java
+++ android/src/main/java/com/reactlibrary/voice_recorder/VoiceUtils.java
@@ -5,6 +5,9 @@ public class VoiceUtils {
     private static final int AMPLITUDE_THRESHOLD = 1500;
 
     public static boolean isHearingVoice(byte[] buffer, int size) {
+        if (buffer == null){
+            return false;
+        }
         for (int i = 0; i < size - 1; i += 2) {
             // The buffer has LINEAR16 in little endian.
             int s = buffer[i + 1];
